f70db5d4afc137f023969a1bf1c9e5a2da93f4b3
Alice Whelan <awhelan.dev@gmail.com>
2020-11-04 09:24:41 -0500

    Fixed JPanel not showing on JFrame

--- src/org/group2/finalproject/Home.java
+++ src/org/group2/finalproject/Home.java
@@ -10,9 +10,8 @@ import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import javax.swing.JTable;
 
-public class Home extends JPanel {
+public class Home extends JPanel{
 	
-	JFrame f;
 	JButton ClientBtn;
 	JButton filmBtn;
 	JButton locationBtn;
@@ -20,24 +19,22 @@ public class Home extends JPanel {
 	JScrollPane sp;
 
 	/**
-	 * Create the panel.
+	 * Create the this.
 	 */
-	public Home() {
-		
-	    f = new JFrame("Home");    
-        JPanel panel=new JPanel();  
-        panel.setBounds(40,80,200,200);  
-		
+	public Home() 
+	{
+		this.setBounds(0,0,800,800);  
+        
         ClientBtn = new JButton("Clients");
 		ClientBtn.setBounds(50,100,80,30); 
-		 ClientBtn.addActionListener(new ActionListener() {
+		ClientBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				// redirect to the clients UI
 				JButton ajouterClient = new JButton("Ajouter client");
 				ajouterClient.setBounds(175,100,180,30);
 			}
 		}); 
-		f.getContentPane().add(ClientBtn);
+		this.add(ClientBtn);
 		
 		filmBtn = new JButton("Films");
 		filmBtn.setBounds(150,100,80,30); 
@@ -46,7 +43,7 @@ public class Home extends JPanel {
 				// redirect to the films UI
 			}
 		}); */
-		f.getContentPane().add(filmBtn);
+		this.add(filmBtn);
 		
 		locationBtn = new JButton("Location");
 		locationBtn.setBounds(250,100,180,30); 
@@ -55,7 +52,7 @@ public class Home extends JPanel {
 				// redirect to the location UI
 			}
 		});
-		f.getContentPane().add(locationBtn);
+		this.add(locationBtn);
 		
 		// Does not show up
 		String data[][]={ {"101","Amit","670000"},    
@@ -65,11 +62,8 @@ public class Home extends JPanel {
 		table_1 = new JTable(data, column);
         sp  = new JScrollPane(table_1);
         sp.setBounds(50, 172, 492, 275);
-        f.getContentPane().add(sp);
-         
-         f.setVisible(true);   
-         f.setSize(800,800);    
-         f.getContentPane().setLayout(null);    
+        this.add(sp);
+		//f.getContentPane().setLayout(null);    
 		
 	}
 }
--- src/org/group2/finalproject/Start.java
+++ src/org/group2/finalproject/Start.java
@@ -1,13 +1,22 @@
 package org.group2.finalproject;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class Start
 {
 	public static void main(String[] args)
 	{
 		//Test test = new Test();
 		//test.setVisible(true); 
-		
-		new Home();
+
+		JFrame f = new JFrame("Home");    
+		f.setSize(800,800);
+
+		JPanel home = new Home();
+        f.setContentPane(home);
+        
+		f.setVisible(true);
 		
 		/** ConnectionDB db = new ConnectionDB();
 		db.connectDB(); */
